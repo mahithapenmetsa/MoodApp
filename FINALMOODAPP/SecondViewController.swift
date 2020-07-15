235d886b7840c3f52365d2becf5e337797a855cc
MAHITHA <mahi.penmetsa@gmail.com>
2020-07-15 11:16:10 -0400

    Initial Commit

---
+++ FINALMOODAPP/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FINALMOODAPP
+//
+//  Created by Mahitha Penmetsa on 7/15/20.
+//  Copyright © 2020 Mahitha Penmetsa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
